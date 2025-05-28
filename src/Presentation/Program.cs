8fad6ca420799ce9875a389ebd9958acc9ae2fc4
nphnamm <nphnamm@gmail.com>
2025-05-28 18:23:29 +0700

    update deploy 1

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -158,7 +158,7 @@ app.MapControllers();
 app.Run();
 
 
-//hehe new deploy
+//hehe new deployyy
 // Custom converter for ExerciseType to accept both string and number
 public class FlexibleExerciseTypeEnumConverter : JsonConverter<ExerciseType>
 {
